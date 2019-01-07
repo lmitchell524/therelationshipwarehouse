5c2ecb28181b7841ce375bc6481c8e1eaacf59fd
lmitchell524 <lmitchell524@gmail.com>
2019-01-07 14:04:06 -0800

    Added shop page and first product

---
+++ themes/phlox-child/page-shop.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Template Name: Shop
+ *
+ * @package    Auxin
+ * @author     averta (c) 2014-2018
+ * @link       http://averta.net
+ */
+get_header(); ?>
+
+    <main id="main" >
+        <div class="aux-wrapper">
+            <div class="aux-container">
+                <div id="primary" class="aux-primary" >
+                    <div class="content" role="main"  >
+                        <!-- <div class="services-hero-container">
+                            <div class="about-us-hero-img"></div>
+                        </div> -->
+                        <section class='main-content'>
+                            <div>
+                                <div class='product'>
+                                    <img class='product-img' src="../../uploads/2018/09/pearl.jpg" />
+                                    <div class='product-info'>
+                                        <h1 class='product-title'>Relationship Matters</h1>
+                                        <h2 class='product-author'>By: Dr. Guy Grimes</h2>
+                                        <p class='product-price'>$15.99</p>
+                                        <div class='product-description'>
+                                            Long-lasting relationships are struggling in the church and throughout all American walks of life. The divorce rate has been declining over the past ten years, but only because there are fewer marriages. People are insecure and tentative when it comes to long-term relationships and marriage commitment. There are fewer and fewer examples of healthy relationships and truly happy marriages.
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="paypal"><input type="hidden" name="cmd" value="_cart"><input type="hidden" name="currency_code" value="USD"><input type="hidden" name="business" value="guy@therelationshipwarehouse.com"><input type="hidden" name="item_name" value="Relationship Matters- By Dr. Guy Grimes"><input type="hidden" name="item_number"><input type="hidden" name="amount" value="15.99"><input type="hidden" name="button_subtype" value="products"><input type="hidden" name="shipping" value="3.25"><input type="hidden" name="add" value="1"><input type="submit" value='Add to Cart' border="0" name="submit" alt="PayPal - The safer, easier way to pay online!"><input type="hidden" name="bn" value="godaddy_hosting_WPS_US"><img alt="" style="border-width: 0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1"></form>
+                                    </div>
+                                </div>
+                                <div class='product'>
+                                    <img class='product-img' />
+                                    <div class='product-info'>
+                                        <h1 class='product-title'></h1>
+                                        <p class='product-price'></p>
+                                        <div class='product-description'>
+
+                                        </div>
+                                    </div>
+                                    <a href='' class='add-to-cart'>Add to Cart</a>
+                                </div>
+                                <div class='product'>
+                                    <img class='product-img' />
+                                    <div class='product-info'>
+                                        <h1 class='product-title'></h1>
+                                        <p class='product-price'></p>
+                                        <div class='product-description'>
+
+                                        </div>
+                                    </div>
+                                    <a href='' class='add-to-cart'>Add to Cart</a>
+                                </div>
+                            </div>
+                        </section>
+
+                    </div><!-- end content -->
+                </div><!-- end primary -->
+            </div><!-- end container -->
+        </div><!-- end wrapper -->
+    </main><!-- end main -->
+
+<?php get_footer(); ?>
